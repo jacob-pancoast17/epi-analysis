a7920c6f2c4622760acd7e295e9463d8f341845e
Jacob <jacob.pancoast17@gmail.com>
2026-05-01 23:31:39 -0400

    Add RSV data set and clean it to reduce the number of rows for further processing in R

---
+++ data_clean.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data_cleaned = (
+    # Read in the CSV
+    pd.read_csv("CDC_Wastewater_Data_for_RSV_20260428.csv")
+
+    # Filter by Vermont to lower # of rows
+    .query("state_territory == 'vt'")
+    )
+
+data_cleaned.to_csv('CDC_RSV_Wastewater_Data.csv', index = False)
